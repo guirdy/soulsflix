a031e874d9b1fc578d7d42b6418e082f2fbcd50c
Guilherme <guilherme.gl1997@gmail.com>
2020-08-02 22:27:19 -0300

    feat: video register

--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,6 +2,17 @@ import config from '../config';
 
 const URL = `${config.URL}/categorias?_embed=videos`;
 
+const getAll = () => {
+  return fetch(URL)
+    .then(async (respostaDoServidor) => {
+      if (respostaDoServidor.ok) {
+        const resposta = await respostaDoServidor.json();
+        return resposta;
+      }
+      throw new Error('Não foi possível pegar os dados :(');
+    });
+};
+
 const getAllWithVideos = () => {
   return fetch(URL)
     .then(async (respostaDoServidor) => {
@@ -15,4 +26,5 @@ const getAllWithVideos = () => {
 
 export default {
   getAllWithVideos,
+  getAll,
 };
